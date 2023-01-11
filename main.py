4eb822df0c16be25136f1d60a0b8b8e3342ab502
rumon5h <mdrumonkhan48519@gmail.com>
2023-01-11 14:36:01 +0600

    create name and password and view

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-pwd = input('What is your password? ')
+master_pwd = input('What is your master password? ')
+
+
+def view():
+    with open('password.txt', 'r') as f:
+        for line in f.readlines():
+            print(line.rstrip())
+
+
+def add():
+    name = input('Account name: ')
+    pwd = input("Password: ")
+
+    with open('password.txt', 'a') as f:
+        f.write(name + "|" + pwd + "\n")
+
+
+while True:
+    mode = input(
+        "Would you like to add a new password or view existing ones (view, add), press q to quit? ").lower()
+    if mode == "q":
+        break
+    if mode == 'view':
+        view()
+    elif mode == 'add':
+        add()
+    else:
+        print("Invalid mode.")
+        continue
