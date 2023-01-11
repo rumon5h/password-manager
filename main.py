68c9a5ea18dc501fac86053eb613b90ea2051d0b
rumon5h <mdrumonkhan48519@gmail.com>
2023-01-11 15:09:10 +0600

    generate a key

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
+from cryptography.fernet import Fernet
+
 master_pwd = input('What is your master password? ')
+
+
+def write_key():
+    key = Fernet.generate_key()
+    with open('key.key', 'wb') as key_file:
+        key_file.write(key)
+
+
 
 
 def view():
     with open('password.txt', 'r') as f:
         for line in f.readlines():
-            print(line.rstrip())
+            data = line.rstrip()
+            user, passw = data.split("|")
+            print("User", user, "Password", passw)
 
 
 def add():
